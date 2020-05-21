43672a3593b64fbf1608b3751719e9df67926397
Leeminish <passwordd@naver.com>
2020-05-21 11:54:22 +0900

    change something

--- Time/sleep.go
+++ Time/sleep.go
@@ -6,6 +6,6 @@ import "time"
 func main()  {
 	fmt.Println("500 millisecond Sleep")
 	time.Sleep(500 * time.Millisecond)
-	fmt.Println("3 second Sleep")
-	time.Sleep(3 * time.Second)
+	fmt.Println("2 second Sleep")
+	time.Sleep(2 * time.Second)
 }
